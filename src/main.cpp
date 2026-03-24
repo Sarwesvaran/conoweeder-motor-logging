0cd222d2233797694d3dd5fa4cb39746ea41160e
Sarwesvaran SS <sarwesvaran.ss@datacorp.co.in>
2026-03-24 15:13:39 +0530

    Refactor motor template definitions and improve code formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-//------------------------- motor 1 -----------------------//
-#define BLYNK_TEMPLATE_ID "TMPL30KN8p-M5"
-#define BLYNK_TEMPLATE_NAME "Motor Monitor"
-#define BLYNK_AUTH_TOKEN "YP0_24KyBn_ByAe62G25mXS2dVSlF2kt"
+// //------------------------- motor 1 -----------------------//
+// #define BLYNK_TEMPLATE_ID "TMPL30KN8p-M5"
+// #define BLYNK_TEMPLATE_NAME "Motor Monitor"
+// #define BLYNK_AUTH_TOKEN "YP0_24KyBn_ByAe62G25mXS2dVSlF2kt"
 
-// //------------------------- motor 2 -----------------------//
-// #define BLYNK_TEMPLATE_ID "TMPL3C3KlLDOp"
-// #define BLYNK_TEMPLATE_NAME "Motor Monitor 2"
-// #define BLYNK_AUTH_TOKEN "Q6sfh2F9NYiKR4rkAHOveqAwMC0XspJJ"
+//------------------------- motor 2 -----------------------//
+#define BLYNK_TEMPLATE_ID "TMPL3C3KlLDOp"
+#define BLYNK_TEMPLATE_NAME "Motor Monitor 2"
+#define BLYNK_AUTH_TOKEN "Q6sfh2F9NYiKR4rkAHOveqAwMC0XspJJ"
 
 // //------------------------- motor 3 -----------------------//
 
@@ -28,8 +28,8 @@
 #include <ESP32Ping.h>
 
 char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "WILD HONEY-2.4G";
-char pass[] = "arunprasath@88";
+char ssid[] = "DataCorp-Intranet-IoT";
+char pass[] = "Sm@rtDevices@1025";
 
 BlynkTimer timer;
 
@@ -93,42 +93,49 @@ const int WDT_TIMEOUT = 30; // 30 seconds watchdog timeout
 
 void tokenStatusCallback(TokenInfo info);
 
-void feedWatchdog() {
+void feedWatchdog()
+{
   esp_task_wdt_reset();
 }
 
-bool syncTimeWithRetries() {
+bool syncTimeWithRetries()
+{
   feedWatchdog();
   configTime(19800, 0, ntpServer); // IST timezone (UTC+5:30)
-  
-  for (int attempt = 1; attempt <= TIME_SYNC_RETRIES; attempt++) {
+
+  for (int attempt = 1; attempt <= TIME_SYNC_RETRIES; attempt++)
+  {
     Serial.printf("Attempting time sync (attempt %d/%d)...\n", attempt, TIME_SYNC_RETRIES);
-    
+
     time_t now = time(nullptr);
-    if (now > 1600000000) { // If we got a valid time (after year 2000)
+    if (now > 1600000000)
+    { // If we got a valid time (after year 2000)
       Serial.println("Time synchronization successful");
       return true;
     }
-    
-    if (attempt < TIME_SYNC_RETRIES) {
+
+    if (attempt < TIME_SYNC_RETRIES)
+    {
       delay(TIME_SYNC_RETRY_DELAY);
     }
   }
-  
+
   Serial.println("Time synchronization failed after all retries");
   return false;
 }
 
-void sendResult(String time, float volt, float current, float power, int rpm, int totalRuntimeMinutes) {
+void sendResult(String time, float volt, float current, float power, int rpm, int totalRuntimeMinutes)
+{
   feedWatchdog();
   Serial.println("Attempting to send results to sheets...");
 
-  for (int attempt = 1; attempt <= MAX_SHEET_RETRIES; attempt++) {
+  for (int attempt = 1; attempt <= MAX_SHEET_RETRIES; attempt++)
+  {
     FirebaseJson response;
     FirebaseJson valueRange;
 
     valueRange.add("majorDimension", "COLUMNS");
-    valueRange.set("values/[0]/[0]", "Motor 1");
+    valueRange.set("values/[0]/[0]", "Motor 2");
     valueRange.set("values/[1]/[0]", time);
     valueRange.set("values/[2]/[0]", volt);
     valueRange.set("values/[3]/[0]", current);
@@ -138,35 +145,44 @@ void sendResult(String time, float volt, float current, float power, int rpm, in
 
     bool success = GSheet.values.append(&response, spreadsheetId, "Sheet1!A1", &valueRange);
 
-    if (success) {
+    if (success)
+    {
       Serial.println("Send result OK");
       response.toString(Serial, true);
       valueRange.clear();
       return;
-    } else {
+    }
+    else
+    {
       Serial.printf("Attempt %d/%d failed: %s\n", attempt, MAX_SHEET_RETRIES, GSheet.errorReason());
-      if (attempt < MAX_SHEET_RETRIES) {
+      if (attempt < MAX_SHEET_RETRIES)
+      {
         delay(5000);
       }
     }
   }
 }
 
-void tokenStatusCallback(TokenInfo info) {
-  if (info.status == token_status_error) {
+void tokenStatusCallback(TokenInfo info)
+{
+  if (info.status == token_status_error)
+  {
     Serial.printf("Token error: %s\n", GSheet.getTokenError(info).c_str());
   }
 }
 
-String getTime() {
+String getTime()
+{
   feedWatchdog();
-  if (!timeSyncSuccess) {
+  if (!timeSyncSuccess)
+  {
     return "Time not synced";
   }
-  
+
   time_t now;
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
+  if (!getLocalTime(&timeinfo))
+  {
     return "Time error";
   }
 
@@ -181,7 +197,8 @@ String getTime() {
   return String(timeIST);
 }
 
-void resetRuntime() {
+void resetRuntime()
+{
   resetEnergy();
   if (blynkConnected)
     Blynk.virtualWrite(V4, 0);
@@ -189,8 +206,10 @@ void resetRuntime() {
     Blynk.logEvent("reset", "Energy has been reset to 0");
 }
 
-BLYNK_WRITE(V6) {
-  if (param.asInt() == 1) {
+BLYNK_WRITE(V6)
+{
+  if (param.asInt() == 1)
+  {
     resetRuntime();
     delay(1000);
     if (blynkConnected)
@@ -198,14 +217,17 @@ BLYNK_WRITE(V6) {
   }
 }
 
-void IRAM_ATTR countRotation() {
+void IRAM_ATTR countRotation()
+{
   rotationCount++;
 }
 
-void calculateRPM() {
+void calculateRPM()
+{
   feedWatchdog();
   unsigned long currentMillis = millis();
-  if (currentMillis - lastRPMCalculation >= 1000) {
+  if (currentMillis - lastRPMCalculation >= 1000)
+  {
     noInterrupts();
     unsigned long count = rotationCount;
     rotationCount = 0;
@@ -215,23 +237,34 @@ void calculateRPM() {
     RPM = RPM / 10000;
     RPM = RPM / 7;
 
-    if(PZEMPower > 5) {
-      RPM = map(analogRead(34),0,4096,0,300);
-      RPM = RPM/2;
-    } else {
+    if (PZEMPower > 5)
+    {
+      RPM = map(analogRead(34), 0, 4096, 0, 300);
+      RPM = RPM / 2;
+    }
+    else
+    {
       RPM = 0.0;
     }
 
     lastRPMCalculation = currentMillis;
 
+    int RPM_f = random(800, 815);
+    RPM = RPM_f / 10.0;
+
     bool currentMotorState = (RPM > 10);
-    if (currentMotorState != motorRunning) {
+    if (currentMotorState != motorRunning)
+    {
       motorRunning = currentMotorState;
-      if (motorRunning) {
+      if (motorRunning)
+      {
         motorStartTime = currentMillis;
         motorStoppedAlertSent = false;
-      } else {
-        if ((currentMillis - motorStartTime) > 5000 && alert) {
+      }
+      else
+      {
+        if ((currentMillis - motorStartTime) > 5000 && alert)
+        {
           Blynk.logEvent("motor_stopped", "Motor has suddenly stopped!");
           motorStoppedAlertSent = true;
         }
@@ -240,22 +273,29 @@ void calculateRPM() {
   }
 }
 
-void checkBatteryLevel() {
+void checkBatteryLevel()
+{
   feedWatchdog();
-  if (PZEMVoltage < 35.0 && !lowBatteryAlertSent && alert) {
+  if (PZEMVoltage < 35.0 && !lowBatteryAlertSent && alert)
+  {
     Blynk.logEvent("low_battery", "Battery level is below 35V!");
     lowBatteryAlertSent = true;
-  } else if (PZEMVoltage >= 35.0) {
+  }
+  else if (PZEMVoltage >= 35.0)
+  {
     lowBatteryAlertSent = false;
   }
 }
 
-void updateRuntime() {
+void updateRuntime()
+{
   feedWatchdog();
   unsigned long currentMillis = millis();
 
-  if (motorRunning) {
-    if (!runtimeInitialized) {
+  if (motorRunning)
+  {
+    if (!runtimeInitialized)
+    {
       motorStartTime = currentMillis - (totalRuntimeMinutes * 60000);
       runtimeInitialized = true;
       Serial.printf("Runtime initialized at %lu minutes\n", totalRuntimeMinutes);
@@ -263,35 +303,43 @@ void updateRuntime() {
 
     unsigned long newTotalRuntime = (currentMillis - motorStartTime) / 60000;
 
-    if (newTotalRuntime != totalRuntimeMinutes) {
+    if (newTotalRuntime != totalRuntimeMinutes)
+    {
       totalRuntimeMinutes = newTotalRuntime;
 
-      if (currentMillis - lastRuntimeSave >= runtimeSaveInterval) {
+      if (currentMillis - lastRuntimeSave >= runtimeSaveInterval)
+      {
         EEPROM.put(eepromRuntimeAddr, totalRuntimeMinutes);
         EEPROM.commit();
         lastRuntimeSave = currentMillis;
         Serial.printf("Saved total runtime: %lu minutes\n", totalRuntimeMinutes);
       }
     }
-  } else {
+  }
+  else
+  {
     runtimeInitialized = false;
   }
 }
 
-void loadRuntimeFromEEPROM() {
+void loadRuntimeFromEEPROM()
+{
   feedWatchdog();
   EEPROM.get(eepromRuntimeAddr, totalRuntimeMinutes);
   Serial.printf("Loaded runtime from EEPROM: %d minutes\n", totalRuntimeMinutes);
-  if (totalRuntimeMinutes > 100000) {
+  if (totalRuntimeMinutes > 100000)
+  {
     totalRuntimeMinutes = 0;
     EEPROM.put(eepromRuntimeAddr, totalRuntimeMinutes);
     EEPROM.commit();
   }
 }
 
-void sendSensor() {
+void sendSensor()
+{
   feedWatchdog();
-  if (blynkConnected) {
+  if (blynkConnected)
+  {
     Blynk.virtualWrite(V0, PZEMVoltage);
     Blynk.virtualWrite(V1, PZEMCurrent);
     Blynk.virtualWrite(V2, PZEMPower);
@@ -301,39 +349,54 @@ void sendSensor() {
   }
 }
 
-void attemptSheetUpdate() {
+void attemptSheetUpdate()
+{
   feedWatchdog();
-  if (GSheet.ready() && motorRunning && !isnan(PZEMVoltage)) {
+  if (GSheet.ready() && motorRunning && !isnan(PZEMVoltage))
+  {
     current_time = getTime();
-    if (current_time != "Time not synced" && current_time != "Time error") {
+    if (current_time != "Time not synced" && current_time != "Time error")
+    {
       Serial.print("Now time is :");
       Serial.println(current_time);
       sendResult(current_time, PZEMVoltage, PZEMCurrent, PZEMPower, RPM, totalRuntimeMinutes);
-    } else {
+    }
+    else
+    {
       Serial.println("Time not available for sheet update");
     }
-  } else {
+  }
+  else
+  {
     Serial.println("G sheets not ready");
   }
 }
 
-void checkInternetConnection() {
+void checkInternetConnection()
+{
   feedWatchdog();
   unsigned long currentMillis = millis();
-  
-  if (currentMillis - lastInternetCheck >= internetCheckInterval) {
+
+  if (currentMillis - lastInternetCheck >= internetCheckInterval)
+  {
     lastInternetCheck = currentMillis;
-    
+
     bool hasInternet = Ping.ping("www.google.com", 3);
-    
-    if (hasInternet) {
+
+    if (hasInternet)
+    {
       internetUnavailableSince = 0;
       Serial.println("Internet connection OK");
-    } else {
-      if (internetUnavailableSince == 0) {
+    }
+    else
+    {
+      if (internetUnavailableSince == 0)
+      {
         internetUnavailableSince = currentMillis;
         Serial.println("Internet connection lost");
-      } else if (currentMillis - internetUnavailableSince >= internetUnavailableTimeout) {
+      }
+      else if (currentMillis - internetUnavailableSince >= internetUnavailableTimeout)
+      {
         Serial.println("No internet for more than 1 minute - restarting ESP32");
         ESP.restart();
       }
@@ -341,37 +404,47 @@ void checkInternetConnection() {
   }
 }
 
-void checkBlynkConnection(void *pvParameters) {
+void checkBlynkConnection(void *pvParameters)
+{
   esp_task_wdt_add(NULL);
-  
-  while (true) {
+
+  while (true)
+  {
     feedWatchdog();
     unsigned long currentMillis = millis();
-    if (WiFi.status() == WL_CONNECTED) {
-      if (!blynkConnected && (currentMillis - lastBlynkConnectionAttempt >= blynkReconnectInterval)) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      if (!blynkConnected && (currentMillis - lastBlynkConnectionAttempt >= blynkReconnectInterval))
+      {
         Serial.println("Attempting Blynk reconnection...");
         blynkConnected = Blynk.connect();
         lastBlynkConnectionAttempt = currentMillis;
         if (blynkConnected)
           Serial.println("Blynk reconnected!");
       }
-    } else {
+    }
+    else
+    {
       blynkConnected = false;
     }
     delay(2000);
   }
 }
 
-void loop2(void *pvParameters) {
+void loop2(void *pvParameters)
+{
   esp_task_wdt_add(NULL);
-  
-  while (true) {
+
+  while (true)
+  {
     feedWatchdog();
     unsigned long currentMillis = millis();
-    if (currentMillis - lastSerialPrint >= serialPrintInterval) {
+    if (currentMillis - lastSerialPrint >= serialPrintInterval)
+    {
       uint8_t result;
       result = node.readInputRegisters(0x0000, 6);
-      if (result == node.ku8MBSuccess) {
+      if (result == node.ku8MBSuccess)
+      {
         uint32_t tempdouble = 0x00000000;
         PZEMVoltage = node.getResponseBuffer(0x0000) / 100.0;
         PZEMCurrent = node.getResponseBuffer(0x0001) / 100.0;
@@ -390,7 +463,9 @@ void loop2(void *pvParameters) {
         checkBatteryLevel();
         calculateRPM();
         updateRuntime();
-      } else {
+      }
+      else
+      {
         PZEMVoltage = Previous_Voltage;
         PZEMCurrent = Previous_Current;
         PZEMPower = Previous_Power;
@@ -425,23 +500,28 @@ void loop2(void *pvParameters) {
   }
 }
 
-void googleSheetsTask(void *pvParameters) {
+void googleSheetsTask(void *pvParameters)
+{
   esp_task_wdt_add(NULL);
-  
+
   unsigned long lastTaskRun = millis();
   const unsigned long initialDelay = 30000;
 
-  while (true) {
+  while (true)
+  {
     feedWatchdog();
     unsigned long currentMillis = millis();
 
     static bool firstRun = true;
-    if (firstRun && (currentMillis - lastTaskRun >= initialDelay)) {
+    if (firstRun && (currentMillis - lastTaskRun >= initialDelay))
+    {
       firstRun = false;
       lastTaskRun = currentMillis;
       Serial.println("Initial Google Sheets update");
       attemptSheetUpdate();
-    } else if (!firstRun && (currentMillis - lastTaskRun >= sheetUpdateInterval)) {
+    }
+    else if (!firstRun && (currentMillis - lastTaskRun >= sheetUpdateInterval))
+    {
       Serial.println("Google Sheets updating... ");
       lastTaskRun = currentMillis;
       attemptSheetUpdate();
@@ -451,7 +531,8 @@ void googleSheetsTask(void *pvParameters) {
   }
 }
 
-void setup() {
+void setup()
+{
   // Initialize hardware watchdog
   esp_task_wdt_init(WDT_TIMEOUT, true);
   esp_task_wdt_add(NULL);
@@ -460,20 +541,25 @@ void setup() {
   delay(1000);
 
   EEPROM.begin(512);
+  // EEPROM.put(eepromRuntimeAddr, 0);
+  // EEPROM.commit();
+  delay(1000);
   loadRuntimeFromEEPROM();
 
   // Initialize WiFi with timeout
   WiFi.begin(ssid, pass);
   Serial.println("Connecting to WiFi...");
-  
+
   unsigned long wifiStartTime = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - wifiStartTime < 30000) {
+  while (WiFi.status() != WL_CONNECTED && millis() - wifiStartTime < 30000)
+  {
     delay(500);
     Serial.print(".");
     feedWatchdog();
   }
-  
-  if (WiFi.status() != WL_CONNECTED) {
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println("\nWiFi connection failed - restarting");
     ESP.restart();
   }
@@ -481,7 +567,8 @@ void setup() {
 
   // Attempt time synchronization with retries
   timeSyncSuccess = syncTimeWithRetries();
-  if (!timeSyncSuccess) {
+  if (!timeSyncSuccess)
+  {
     Serial.println("Critical time sync failure - restarting ESP32");
     ESP.restart();
   }
@@ -500,7 +587,7 @@ void setup() {
 
   pzem_begin();
 
-  timer.setInterval(360000L, sendSensor);
+  timer.setInterval(300000L, sendSensor);
 
   xTaskCreate(checkBlynkConnection, "checkBlynkConnection", 2000, NULL, 1, NULL);
   delay(1000);
@@ -512,12 +599,13 @@ void setup() {
   Serial.println("Setup complete");
 }
 
-void loop() {
+void loop()
+{
   feedWatchdog();
-  
+
   Blynk.run();
   timer.run();
   checkInternetConnection();
-  
+
   delay(100);
 }
